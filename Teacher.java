0ad902f62b4ff7a65a65ee8017fd3a3e3640f939
Manasa000985 <manasacs02@gmail.com>
2024-10-14 22:15:25 +0530

    check result

---
+++ Teacher.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Teacher extends person 
+{
+	String subject;
+	 public void setsubject1(String subject) 
+	 {
+	        this.subject = subject;
+	    }
+	    public void display()
+	    {
+	    	 System.out.println("Name: " + name + ", Age: " + age + ", Subject: " + subject);
+	    }
+	}
